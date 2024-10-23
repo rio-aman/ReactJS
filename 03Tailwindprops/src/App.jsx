b0a878196a5fdf8b946e7bee2e152892077f8faf
rio-aman <amprajapati08893.com>
2024-10-23 19:43:14 +0530

    learning about props and handeling components of tailwind

--- 03Tailwindprops/src/App.jsx
+++ 03Tailwindprops/src/App.jsx
@@ -2,13 +2,21 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-
+import Card from './components/card'
 function App() {
+  let myobj = {
+    username: "AMAN",
+    age: 21
+  }
+
+  let newArr = [1,3,6]
+
 
   return (
     <>
-    <h1 className='bg-green-400'>Tailwind Test</h1>
-    
+      <h1 className='bg-green-400'>Tailwind Test</h1>
+      <Card username="chai aur code" someobj={newArr} />
+      <Card username="Aman" />
     </>
   )
 }
