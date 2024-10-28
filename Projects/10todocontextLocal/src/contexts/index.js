438ae34f13e36fa577b9cb763fe5a007ead99128
rio-aman <amprajapati08893.com>
2024-10-28 20:34:52 +0530

    learning more about context api by creating a todolist project

---
+++ Projects/10todocontextLocal/src/contexts/index.js
@@ -0,0 +1,4 @@
+// why do we created this index.js file 
+// bcz so then we can import all the things here and if any other file need this then we can import this file then all the things we can use also 
+
+export {TodoContext, TodoProvider, useTodo} from "./TodoContext"
